b5628bcca56a86a588cba98ebe57a2c40a2e52c0
shifa11nadaf <101701065+shifa11nadaf@users.noreply.github.com>
2022-10-05 22:25:10 +0530

    Add files via upload

---
+++ Template-Online-resume-creator-website-main/rs.sql
@@ -0,0 +1,33 @@
+
+
+
+
+create table Personal Information(
+    Your_Name varchar(50),
+    Your_Email varchar(50),
+    Contact_No varchar(12),
+    Your_Address varchar(50)
+    
+);
+
+create table social Links(
+   Github varchar(30),
+   Linkedin varchar(20)
+     
+);
+
+create table Internships(
+    1 varchar(50),
+    2 varchar(50)
+    
+);
+
+create table Professional Information(
+    Job_Title varchar(50),
+    Work_Experience varchar(50),
+    Education varchar(200),
+    Projects varchar(200),
+    Skills varchar(50),
+    Interests varchar(30)
+);
+
